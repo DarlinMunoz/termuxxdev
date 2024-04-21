d5c3dafd7f0dfe981ea807c794ebfcfba87c993f
DarlinMunoz <darlin.munoz.morales@gmail.com>
2024-04-21 05:39:14 -0500

    add new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { Articles } from './pages/Articles';
+import { Courses } from './pages/Courses';
+import { About } from './pages/About';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/articles' element={<Articles />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );
